55035b02945706fd12f4e7981be7cb0515e9b91b
Nathan Nunes <nunesxd7@hotmail.com>
2021-09-07 22:53:17 -0300

    terminando exercicios da introducao js

--- fundamentos/Introducao_JS_e_Logica_de_Programacao/dia_4/scripts.js
+++ fundamentos/Introducao_JS_e_Logica_de_Programacao/dia_4/scripts.js
@@ -48,14 +48,64 @@ for (let key in info) {
 
 
 ////////////////////////////////////////////////////////////////
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+function palindrome(eounaoe) {
+  let pali = eounaoe.split("")
+  let drome = pali.reverse()
+  drome = drome.join("")
+  if (eounaoe == drome) {
+    return true
+  } else {
+    return false
+  }
+}
 
 
-let eounaoe = ["avc"]
-eounaoe.split()
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
-// function palindrome(eounaoe) {
-//   for (const iterator of eounaoe) {
-//     console.log(interator);
-//   }
+function maxNum(arr) {
+  let max = Math.max(...arr)
+  let theMaxNum = arr.indexOf(max)
+  return theMaxNum
+}
 
-// console.log(palindrome("arara"));
+
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+function minNum(arr) {
+  let min = Math.min(...arr)
+  let theMinNum = arr.indexOf(min)
+  return theMinNum
+}
+
+console.log(minNum([2, 4, 6, 7, 10, 0, -3]));
+
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+function biggestName(arr) {
+  let biggest = ""
+  for (const key in arr) {
+    if (arr[key].length > biggest.length) {
+    biggest = arr[key]
+  } 
+} return biggest;
+}
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+function count(arr) {
+  arr.sort()
+  let result = []
+  for (let y in arr) {
+    let resultTest = []
+    resultTest.push(arr[y])
+  for (const key in arr) {
+    if (arr[key] == resultTest[0]) {
+      resultTest.push(arr[key])
+    } else if (key == (arr.length - 1) && resultTest.length > result.length){
+      result = resultTest
+    }
+    }
+  }return result[0]
+ }
