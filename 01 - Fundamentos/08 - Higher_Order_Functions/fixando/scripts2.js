17f8721aa2f9702c47ce42a53264628bcbad77c4
NunesNathan <nunesxd7@hotmail.com>
2021-10-05 12:48:08 -0300

    exercicios finalizados

---
+++ 01 - Fundamentos/08 - Higher_Order_Functions/fixando/scripts2.js
@@ -0,0 +1,11 @@
+const wakeUp = () => 'Acordando!!';
+const takeShower = () => 'Bora tomar café!!';
+const bedTime = () => 'Partiu dormir!!';
+
+const doingThings = (param) => {
+  console.log(param);
+}
+
+doingThings(wakeUp());
+doingThings(takeShower());
+doingThings(bedTime());
